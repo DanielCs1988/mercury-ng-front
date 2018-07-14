5ee028e55b6f38d3203c1254689850fb4a62fcda
Emperor <daniel.cs20@gmail.com>
2018-07-15 01:21:58 +0200

    Attempt 11. Added dirname to path because: TypeError: path must be absolute or specify root to res.sendFile

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/Mercury-NG'));
+app.use(express.static(__dirname + '/dist/Mercury-NG'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join('./dist/Mercury-NG/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Mercury-NG/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
