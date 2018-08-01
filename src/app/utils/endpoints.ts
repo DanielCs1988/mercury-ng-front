15ac7ed196fc8f74e1472ec00b91983bf3c2da7b
DanielCs88 <daniel.cs20@gmail.com>
2018-08-01 22:56:49 +0200

    Changing address

--- src/app/utils/endpoints.ts
+++ src/app/utils/endpoints.ts
@@ -1,7 +1,7 @@
 export const enum Endpoints {
     FEED_HTTP = 'https://protected-island-21893.herokuapp.com/',
     FEED_WS = 'wss://protected-island-21893.herokuapp.com/',
-    CHAT = 'ws://localhost:8080',
+    CHAT = 'ws://192.168.1.106:8080',
     EVENTS = 'https://mercury-events.herokuapp.com/events',
     ARTICLES = 'https://mercury-news.herokuapp.com/articles/',
     WEATHER = 'https://mercury-news.herokuapp.com/weather'
